91b287e7bddea21ece762fd3697362bea16ae5f0
Sebastian Smiley <sebastianswms@gmail.com>
2024-02-06 12:44:07 -0500

    Better tests

--- test_objects.py
+++ test_objects.py
@@ -3,18 +3,17 @@ import math
 import pytest
 
 def test_point():
-    my_point = objects.Point(dimensions=2, coordinates=[3,4])
-    assert my_point.dimensions == 2
-    assert my_point.coordinate() == [3,4]
-    assert my_point.coordinate(dimension=0) == 3
-    assert my_point.coordinate(dimension=1) == 4
+    my_point = objects.Point(dimensions=2, coordinates=(3,4))
+    assert my_point.get_dimensions() == 2
+    assert my_point.get_coordinates() == (3,4)
+    assert my_point.get_coordinates(dimension=0) == 3
+    assert my_point.get_coordinates(dimension=1) == 4
 
-    another_point = objects.Point(dimensions=2, coordinates=[0,0])
-    assert math.isclose(objects.Point.distance_between(my_point, another_point), 5.0)
+    assert math.isclose(objects.Point.distance_between(my_point), 5.0)
+    another_point = objects.Point(dimensions=2, coordinates=(8,16))
+    assert math.isclose(objects.Point.distance_between(my_point, another_point), 13.0)
 
-    with pytest.raises(Exception):
-        my_point.distance_from(12)
-    with pytest.raises(Exception):
-        my_point.coordinate(dimension=2)
-    with pytest.raises(Exception):
-        objects.Point(dimensions=2, coordinates=[3,4,5])
+    with pytest.raises(ValueError):
+        my_point.get_coordinates(dimension=2)
+    with pytest.raises(ValueError):
+        objects.Point(dimensions=2, coordinates=(3,4,5))
