31f2a225b000252898cbe54b86fc60fdab5e5dbe
Sebastian Smiley <46581584+sebastianswms@users.noreply.github.com>
2024-02-22 02:05:20 +0000

    Test for closest pair

--- test_objects.py
+++ test_objects.py
@@ -26,3 +26,22 @@ def test_circle():
     assert math.isclose(my_circle.area(), 25 * math.pi)
     assert math.isclose(my_circle.circumference(), 10 * math.pi)
     assert math.isclose(my_circle.diameter(), 10)
+
+def test_closest_pair()
+
+    a = o.Point(2,(0,0))
+    b = o.Point(2,(3,4))
+    c = o.Point(2,(0,8))
+    d = o.Point(2,(8,5))
+    e = o.Point(2,(12,3))
+    f = o.Point(2,(9,16))
+    g = o.Point(2,(9,16))
+
+    manager = Manager("args")
+    with pytest.raises(ValueError):
+        assert manager.closest_pair(set(a))
+    assert manager.closest_pair(set(a,b)) == set(a,b)
+    with pytest.raises(RuntimeError):
+        assert manager.closest_pair(set(a,b,c))
+    assert manager.closest_pair(set(a,b,c,d,e,f)) == set(d,e)
+    assert manager.closest_pair(set(a,b,c,d,e,f,g)) == set(f,g)
